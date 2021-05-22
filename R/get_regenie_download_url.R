95878966bfd3ce60cc1f8f9e33e7c7c6d526eaee
richelbilderbeek <richel@richelbilderbeek.nl>
2021-05-22 11:18:19 +0200

    Add 'get_regenie_download_url'

---
+++ R/get_regenie_download_url.R
@@ -0,0 +1,8 @@
+#' Get the REGENIE download URL
+#' @export
+get_regenie_download_url <- function() {
+  paste0(
+    "https://github.com/rgcgithub/regenie/releases/download/",
+    "v2.0.2/regenie_v2.0.2.gz_x86_64_Linux.zip"
+  )
+}
